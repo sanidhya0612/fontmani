822e47b95a0d7d0f8d6a9b3114cff190a6aee2f3
sanidhya0612 <102963033+sanidhya0612@users.noreply.github.com>
2023-08-20 11:56:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,28 +1,45 @@
-var songa = "";
-var songb = "";
+leftWristX = "";
+rightWristX = "";
+difference = "";
 
 function preload()
-{
-    songa = loadSound("Harry-Potter-Theme.mp3");
-    songb = loadSound("Kate_Bush_-_Running_Up_That_Hill.mp3");
-}
+{}
 
 function setup()
 {
-    canvas = createCanvas(600,500);
-    canvas.center();
-
     video = createCapture(VIDEO);
-    video.hide();
+    video.size(500,500);
+
+    canvas = createCanvas(500,500);
+    canvas.position(560,150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);    
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet is initialized !!!');
 }
 
 function draw()
 {
-    image(video, 0, 0, 600, 500,);
+    background('#610e78');
+    textSize(difference);
+    fill("red");
+    text("Sanidhya Gupta", 20, 200);
 }
 
-function play()
+function gotPoses(results)
 {
-    songa.play();
-    songb.play();
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = Math.floor(leftWristX - rightWristX);
+
+
+    }
 }
+
